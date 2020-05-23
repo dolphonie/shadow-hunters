bb888369d5e475ce0bf54de66e0a6192b8a853cb
amritrau <amritrau@gmail.com>
2020-05-23 09:34:03 -0700

    Add DevRandomAgent for debugging and RLAgent shell

--- shadow-hunters/agent.py
+++ shadow-hunters/agent.py
@@ -33,4 +33,54 @@ class RandomAgent(AgentInterface):
         return (random.random() <= reveal_chance)
 
 
-Agent = RandomAgent
+class DevRandomAgent(AgentInterface):
+    """Defines an agent that randomly interacts with the game"""
+
+    def choose_action(self, options, player, gc):
+        """Choose an action from the options provided"""
+
+        print("\n" * 4)
+        print("=========================")
+        print(f"PLAYER {player.user_id}")
+        print("=========================")
+        print("-----")
+        print(f"Options")
+        print("-----")
+        print(options)
+        public_state, private_state = gc.dump()
+        priv = [p for p in private_state if p['user_id'] == player.user_id]
+        print("-----")
+        print(f"Public state")
+        print("-----")
+        print(json.dumps(public_state['players'], indent=4))
+        print("-----")
+        print(f"Private state")
+        print("-----")
+        print(json.dumps(priv, indent=4))
+        if 'Decline' in options and len(options) > 1:
+            options.remove('Decline')
+        return {'value': random.choice(options)}
+
+    def choose_reveal(self, player, gc):
+        """Return true if the agent chooses to reveal"""
+        reveal_chance = gc.round_count / 20
+        return (random.random() <= reveal_chance)
+
+
+class RLAgent(AgentInterface):
+    """Defines an RL agent that interacts with the game"""
+
+    def choose_action(self, options, player, gc):
+        """Choose an action from the options provided"""
+        if len(options) == 1:
+            # If there's only one possible option, don't think too hard :-)
+            return {'value': options[0]}
+
+        pass
+
+    def choose_reveal(self, player, gc):
+        """Return true if the agent chooses to reveal"""
+        pass
+
+
+Agent = DevRandomAgent
